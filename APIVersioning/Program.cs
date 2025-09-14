aaf0fd4a9355e61cb269fd1788620fc3b00c5c78
Ravikumar Makwana <17comp.ravi.makwana@gmail.com>
2025-09-14 14:53:02 +0530

    Configure API Versioning.

--- APIVersioning/Program.cs
+++ APIVersioning/Program.cs
@@ -1,4 +1,20 @@
+using Asp.Versioning;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApiVersioning(options =>
+{
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.ReportApiVersions = true; // Return the supported versions in response headers
+
+    // Combine multiple version readers to support different versioning schemes
+    options.ApiVersionReader = ApiVersionReader.Combine(
+        new QueryStringApiVersionReader("api-version"),    // e.g., ?api-version=1.0
+        new HeaderApiVersionReader("X-API-VERSION"),       // e.g., X-API-VERSION: 1.0
+        new UrlSegmentApiVersionReader()                   // e.g., /v1.0/
+        );
+});
 
 // Add services to the container.
 
